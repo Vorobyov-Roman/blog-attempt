be7196e43fb74d5497111931f76b35694fded6ba
Vorobyov-Roman <vorobrawr@gmail.com>
2015-07-06 16:00:43 +0300

    newlines

--- js/app.js
+++ js/app.js
@@ -1,8 +1,9 @@
-angular.module('blog', ['ngMaterial', 'ngRoute'])
-	.config(['$routeProvider', function ($routeProvider) {
+angular.module('blog', ['ngRoute', 'ngMaterial'])
+	.config(['$routeProvider', function ($routeProvider, $mdThemingProvider) {
 		$routeProvider
 			.when('/home', { templateUrl: 'views/home.html', controller: 'homeCtrl' })
 			.when('/posts', { templateUrl: 'views/posts.html' })
 			.when('/users', { templateUrl: 'views/users.html' })
 			.otherwise({ redirectTo: '/home' });
 	}]);
+	
--- js/controllers/homeController.js
+++ js/controllers/homeController.js
@@ -2,3 +2,4 @@ angular.module('blog')
 	.controller('homeCtrl', ['$scope', '$http', function ($scope, $http) {
 		$scope.name = 'works';
 	}]);
+	
