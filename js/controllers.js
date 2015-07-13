4928506024a112e45b2fc2d23a3914b61c5ece02
Vorobyov-Roman <vorobrawr@gmail.com>
2015-07-14 01:08:12 +0300

    data handling is on the client now

--- js/controllers.js
+++ js/controllers.js
@@ -1,28 +1,37 @@
 blog.controller('homeCtrl', ['$scope', '$http', function($scope, $http) {
 	$http.get('http://178.165.53.183:3000/api/blog/posts?showuser=1')
 		.success(function(data) {
+			var monthNames = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+			Date.prototype.toDateFormatted = function() {
+				return monthNames[this.getMonth()] + ' ' + this.getDate() + ', ' + this.getFullYear();
+			};
+			Date.prototype.toTimeFormatted = function(format) {
+				if (format == 12) {
+					return this.getHours() % 12 + ':' + this.getMinutes() + (this.getHours() / 12 < 1 ? 'am' : 'pm');
+				} else {
+					return this.getHours() + ':' + this.getMinutes();
+				}
+			};
+
 			$scope.posts = data;
 
 			$scope.posts.forEach(function(post) {
 				function isToday(date) {
-					return new Date(Date.now()).getDate() == new Date(post.created.date).getDate();
+					return new Date(date).getDate() == new Date(Date.now()).getDate();
 				}
 
-				if (JSON.stringify(post.edited) == JSON.stringify(post.created)) {
+				if (new Date(post.created).toString() == new Date(post.edited).toString()) {
 					delete post.edited;
 				}
-				
-				if (isToday(post.created)) {
-					post.datePreposition = 'at';
-					post.created = post.created.time;
-				} else {
-					post.datePreposition = 'on';
-					post.created = post.created.date;
-				}
 
+				post.created = isToday(post.created) ?
+					'at ' + new Date(post.created).toTimeFormatted(24) :
+					'on ' + new Date(post.created).toDateFormatted();
 				
 				if (typeof post.edited !== 'undefined') {
-					post.edited = isToday(post.edited) ? post.edited.time : post.edited.date;
+					post.edited = isToday(post.edited) ?
+						'at ' + new Date(post.edited).toTimeFormatted(24) :
+						'on ' + new Date(post.edited).toDateFormatted();
 				}
 			});
 		})
