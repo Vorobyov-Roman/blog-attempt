87cd86fd847a7a139c7b87574655be8cf2845ae6
Vorobyov-Roman <vorobrawr@gmail.com>
2015-07-14 03:35:24 +0300

    links to post pages

--- js/controllers.js
+++ js/controllers.js
@@ -1,38 +1,42 @@
+function FormatPost(post) {
+	var monthNames = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+	Date.prototype.toDateFormatted = function() {
+		return monthNames[this.getMonth()] + ' ' + this.getDate() + ', ' + this.getFullYear();
+	};
+	Date.prototype.toTimeFormatted = function(format) {
+		if (format == 12) {
+			return this.getHours() % 12 + ':' + this.getMinutes() + (this.getHours() / 12 < 1 ? 'am' : 'pm');
+		} else {
+			return this.getHours() + ':' + this.getMinutes();
+		}
+	};
+
+	function isToday(date) {
+		return new Date(date).getDate() == new Date(Date.now()).getDate();
+	}
+
+	if (new Date(post.created).toString() == new Date(post.edited).toString()) {
+		delete post.edited;
+	}
+
+	post.created = isToday(post.created) ?
+		'at ' + new Date(post.created).toTimeFormatted(24) :
+		'on ' + new Date(post.created).toDateFormatted();
+	
+	if (typeof post.edited !== 'undefined') {
+		post.edited = isToday(post.edited) ?
+			'at ' + new Date(post.edited).toTimeFormatted(24) :
+			'on ' + new Date(post.edited).toDateFormatted();
+	}
+}
+
 blog.controller('homeCtrl', ['$scope', '$http', function($scope, $http) {
 	$http.get('http://178.165.53.183:3000/api/blog/posts?showuser=1')
 		.success(function(data) {
-			var monthNames = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
-			Date.prototype.toDateFormatted = function() {
-				return monthNames[this.getMonth()] + ' ' + this.getDate() + ', ' + this.getFullYear();
-			};
-			Date.prototype.toTimeFormatted = function(format) {
-				if (format == 12) {
-					return this.getHours() % 12 + ':' + this.getMinutes() + (this.getHours() / 12 < 1 ? 'am' : 'pm');
-				} else {
-					return this.getHours() + ':' + this.getMinutes();
-				}
-			};
-
 			$scope.posts = data;
 
 			$scope.posts.forEach(function(post) {
-				function isToday(date) {
-					return new Date(date).getDate() == new Date(Date.now()).getDate();
-				}
-
-				if (new Date(post.created).toString() == new Date(post.edited).toString()) {
-					delete post.edited;
-				}
-
-				post.created = isToday(post.created) ?
-					'at ' + new Date(post.created).toTimeFormatted(24) :
-					'on ' + new Date(post.created).toDateFormatted();
-				
-				if (typeof post.edited !== 'undefined') {
-					post.edited = isToday(post.edited) ?
-						'at ' + new Date(post.edited).toTimeFormatted(24) :
-						'on ' + new Date(post.edited).toDateFormatted();
-				}
+				FormatPost(post);
 			});
 		})
 		.error(function() {
@@ -40,11 +44,16 @@ blog.controller('homeCtrl', ['$scope', '$http', function($scope, $http) {
 		});
 }]);
 
-blog.controller('userCtrl', ['$scope', '$http', function($scope, $http) {
-	alert('Imagine being taken to the user profile');
+blog.controller('postCtrl', ['$scope', '$http', '$routeParams', function($scope, $http, $routeParams) {
+	$http.get('http://178.165.53.183:3000/api/blog/posts/' + $routeParams.id)
+		.success(function(data) {
+			$scope.post = data;
+
+			FormatPost($scope.post);
+		});
 }]);
 
-blog.controller('postCtrl', ['$scope', '$http', function($scope, $http) {
-	alert('Imagine being taken to the post page');
+blog.controller('userCtrl', ['$scope', '$http', function($scope, $http) {
+	alert('Imagine being taken to the user profile');
 }]);
 	
